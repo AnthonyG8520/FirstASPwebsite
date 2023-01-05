473feec0fe3c4663612e73e3350e581b786d3291
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2023-01-05 15:30:51 -0600

    created models folder and added json file for project data to be stored

---
+++ FirstASPwebsite/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace FirstASPwebsite.Models
+{
+    public class Post
+    {
+        public long Id { get; set; }
+
+        public String ImageUrl { get; set; }
+
+        public int Description { get; set; }
+
+        public override string ToString(){
+           return JsonSerializer.Serialize<Post>(this);
+        }
+
+    }
+}
